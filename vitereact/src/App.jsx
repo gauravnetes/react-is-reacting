b0f25eb3d8047adb3c69a031a877be55871252b4
gauravnetes <gouravchandra935@gmail.com>
2024-10-17 22:45:44 +0530

    initial commit

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+
+    <Chai/>
+    <h1>Chai aur React</h1>
+    <p>test paragraph</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h2>React is Ready</h2>
+    )
+}
+export default Chai
